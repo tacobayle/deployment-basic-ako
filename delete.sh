465def6e1dff4a88be094342e33f0cebeef22962
tacobayle <nicolas.bayle@gmail.com>
2021-11-24 16:48:52 +0000

    20211124_post_update_api_server_ip

--- delete.sh
+++ delete.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #arcade_username=nbayle
-http_response=$(curl -X DELETE -H "Content-Type: application/json" -s -d '{"username": "'"$arcade_username"'"}' http://worker:5000/deployment-basic-ako)
+http_response=$(curl -X DELETE -H "Content-Type: application/json" -s -d '{"username": "'"$arcade_username"'"}' http://172.17.0.1:5000/deployment-basic-ako)
 status=$?
 if [ "$status" -eq 0 ]; then
   echo $(echo $http_response | jq -r .message)
