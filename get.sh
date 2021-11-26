7e1328d9ad04698ad7bc21fef51985308e19ea22
tacobayle <nicolas.bayle@gmail.com>
2021-11-26 10:56:49 +0000

    20211126_no_ssh_jump

--- get.sh
+++ get.sh
@@ -3,6 +3,7 @@
 http_response=$(curl -X GET -H "Content-Type: application/json" -s -d '{"username": "'"$arcade_username"'"}' http://172.17.0.1:5000/deployment-basic-ako)
 status=$?
 if [ "$status" -eq 0 ]; then
+  cat ascii-art.ako-basic
   result=$(echo $http_response | jq .message | tr -d '"')
   echo -e $result
 else
